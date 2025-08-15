4d34ef65e7cb5fae8b0608eafff72068c7022722
Muhammad-Hashir313 <108664836+Muhammad-Hashir313@users.noreply.github.com>
2025-08-15 15:28:23 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = 3000
 const app = express()
 
 app.get('/', (req, res) => {
-    res.json({ message: "Production is working fine :)" })
+    res.json({ message: "Working Fine Right?" })
 })
 
 app.listen(PORT, '0.0.0.0', () => console.log("App started on localhost:", PORT))
